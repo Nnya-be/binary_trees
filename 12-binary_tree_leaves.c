5b604810cfced11896bc770fd92513a58f374fc6
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-01-05 11:24:09 +0000

    Created: task 12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_leaves - Function to count the leaves in a binary tree.
+ *@tree: The binary tree to count.
+ *Return: NULL if a null tree else the number of leaves.
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
