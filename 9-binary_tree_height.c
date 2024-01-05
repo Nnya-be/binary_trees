2e66abc0621e0ea3d238da7c7e12b05c8a388d87
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-01-05 01:45:53 +0000

    MOdified: betty

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,12 +8,12 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0;
-	if (tree == NULL)
-		return (0);
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
-    if (tree && tree->right == NULL && tree->left == NULL)
-    	return (0);
-    return ((left_height > right_height ? left_height : right_height) + 1);
+size_t left_height = 0, right_height = 0;
+if (tree == NULL)
+return (0);
+left_height = binary_tree_height(tree->left);
+right_height = binary_tree_height(tree->right);
+if (tree && tree->right == NULL && tree->left == NULL)
+return (0);
+return ((left_height > right_height ? left_height : right_height) +1);
 }
