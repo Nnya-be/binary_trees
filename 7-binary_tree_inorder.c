ef5f1d06d3bb58305e1d7acbc0276f6274174196
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-01-05 01:32:35 +0000

    Modified

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -4,11 +4,11 @@
  *@tree: The tree to traverse.
  *@func: The function to pass.
  */
- void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
- {
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
 	if (tree == NULL || func == NULL)
 		return;
 	binary_tree_inorder(tree->left, func);
 	func(tree->n);
-	binary_tree_inorder(tree->right, func); 
- }
+	binary_tree_inorder(tree->right, func);
+}
