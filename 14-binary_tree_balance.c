35cddf2c7a99b44e8aeb33e6168709d138c9da80
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-01-05 19:38:23 +0000

    Reduced: the number of functions in the file

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,20 +14,3 @@ int binary_tree_balance(const binary_tree_t *tree)
 	right_height = binary_tree_height(tree->right);
 	return ((int)(left_height - right_height));
 }
-
-/**
- *binary_tree_height - Measures the height of the tree.
- *@tree: The tree pointer.
- *Return: height of the binary tree.
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t left_height, right_height;
-
-	if (!tree)
-		return (0);
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return ((left_height > right_height ? left_height : right_height) + 1);
-}
--- binary_trees.h
+++ binary_trees.h
@@ -45,5 +45,20 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 double power(double base, int expo);
 
 
+/**
+ *binary_tree_height - Measures the height of the tree.
+ *@tree: The tree pointer.
+ *Return: height of the binary tree.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
 
+	if (!tree)
+		return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return ((left_height > right_height ? left_height : right_height) + 1);
+}
 #endif /* _BINARY_TREES_H_ */
